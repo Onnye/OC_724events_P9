90b6bbfd6ff81c79d6b0f2570e9851dac6846d30
Laura TUFO <laura.tufo94@gmail.com>
2024-07-21 15:52:28 +0200

    Fix Select component state management and improve EventList filtering

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,21 +11,22 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const filteredEvents = (
     (!type
       ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+      : data?.events.filter((event) => event.type === type)) || []
+  ) // Filtrer les événements en fonction du type sélectionné
+    .filter((event, index) => {
+      if (
+        (currentPage - 1) * PER_PAGE <= index &&
+        PER_PAGE * currentPage > index
+      ) {
+        return true;
+      }
+      return false;
+    });
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
